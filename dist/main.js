72116404a853967cf22b7ae986382f162c933808
Nick <119859245+nicklySS@users.noreply.github.com>
2024-02-16 12:51:58 +0300

    Npm and webpack were initialized and the gitignore file was created

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document.body.innerHTML+="Увыфвфывыфвыфвыф";
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDRDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdHJhdW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiZG9jdW1lbnQuYm9keS5pbm5lckhUTUwrPVwi0KPQstGL0YTQstGE0YvQstGL0YTQstGL0YTQstGL0YRcIjsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
